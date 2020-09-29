e3a7567d22fac372d2fe1b51eefb69c95c1dc855
Aleksandr Zubakin <eldoson@yandex.ru>
2020-09-29 18:57:55 +0500

    feat: implement all functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (arguments[0] === undefined) return 0;
+    const arr = Array.from(arguments[0]);
+    if (arr.length === 0) return 0;
+    let min = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < min) min = arr[i];
+    }
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (arguments[0] === undefined) return 0;
+    const arr = Array.from(arguments[0]);
+    if (arr.length === 0) return 0;
+    let max = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) max = arr[i];
+    }
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+
+    if (arguments[0] === undefined) return 0;
+    const arr = Array.from(arguments[0]);
+    if (arr.length === 0) return 0;
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
 }
+
+//console.log(exports.min());
